e5c631a5c030ce84e4ba9c7a7c19dfbba848de99
montalfu <felix.montalfu@gmail.com>
2020-09-05 22:44:25 +0700

    V 14 Major

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $(".question_1").click(function(){
+        $(".answere1").show(1000);
+        setTimeout(function(){
+            $(".answere1").hide(1500);
+        }, 3000);
+    });
+    $(".question_2").click(function(){
+        $(".answere2").show(1000);
+        setTimeout(function(){
+            $(".answere2").hide(1500);
+        }, 3000);
+    });
+    $(".question_3").click(function(){
+        $(".answere3").show(1000);
+        setTimeout(function(){
+            $(".answere3").hide(1500);
+        }, 3000);
+    });
+    $(".question_4").click(function(){
+        $(".answere4").show(1000);
+        setTimeout(function(){
+            $(".answere4").hide(1500);
+        }, 3000);
+    });
+    $(".question_5").click(function(){
+        $(".answere5").show(1000);
+        setTimeout(function(){
+            $(".answere5").hide(1500);
+        }, 3000);
+    });
+    $(".question_6").click(function(){
+        $(".answere6").show(1000);
+        setTimeout(function(){
+            $(".answere6").hide(1500);
+        }, 3000);
+    });
+});
