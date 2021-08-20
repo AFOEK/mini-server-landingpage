2a846d8884b751c640e24706eaddb0289685d16d
AFOEK <felix.montalfu@gmail.com>
2021-08-20 11:39:38 +0700

    Adding new convo

--- script.js
+++ script.js
@@ -39,6 +39,38 @@ $(document).ready(function(){
         $(".dark_answere").show(1000);
         setTimeout(function(){
             $(".dark_answere").hide(2500);
-        },4500);
+        }, 3000);
+    });
+    $(".thing_gif").click(function(){
+        $(".thingy_question_1").toggle(1000);
+        $(".thingy_question_2").toggle(1000);
+        $(".thingy_question_3").toggle(1000);
+        $(".thingy_question_4").toggle(1000);
+    });
+    $(".thingy_question_1").click(function(){
+        $(".answere_thingy_1").show(1000);
+        setTimeout(function(){
+            $(".answere_thingy_1").hide(3500);
+        }, 3000);
+    });
+    $(".thingy_question_2").click(function(){
+        $(".answere_thingy_2").show(1000);
+        $(".hidden_answere").show(1000);
+        setTimeout(function(){
+            $(".answere_thingy_2").hide(2500);
+            $(".hidden_answere").hide(3500);
+        }, 3000);
+    });
+    $(".thingy_question_3").click(function(){
+        $(".answere_thingy_3").show(1000);
+        setTimeout(function(){
+            $(".answere_thingy_3").hide(3500);
+        }, 3000);
+    });
+    $(".thingy_question_4").click(function(){
+        $(".answere_thingy_4").show(1000);
+        setTimeout(function(){
+            $(".answere_thingy_4").hide(3500);
+        }, 3000);
     });
 });
