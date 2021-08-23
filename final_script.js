e2589f8b78a96b2ee45e5053eaa135401b0631ed
AFOEK <52521676+AFOEK@users.noreply.github.com>
2021-08-23 14:12:20 +0700

    Minor fix

--- final_script.js
+++ final_script.js
@@ -1,23 +1,23 @@
+(function (global) {
+    if(typeof (global) === "undefined") {
+        throw new Error("window is undefined");
+    }
+    var _hash = "!";
+    var noBackPlease = function () {
+        global.location.href += "#";
+        global.setTimeout(function () {
+            global.location.href += "!";
+        }, 50);
+    };
+    global.onhashchange = function () {
+        if (global.location.hash !== _hash) {
+            global.location.hash = _hash;
+        }
+    };
+    global.onload = function () {
+        noBackPlease();
+    }
+})(window);
 $(document).ready(function(){
-    (function (global) {
-        if(typeof (global) === "undefined") {
-            throw new Error("window is undefined");
-        }
-        var _hash = "!";
-        var noBackPlease = function () {
-            global.location.href += "#";
-            // Making sure we have the fruit available for juice (^__^)
-            global.setTimeout(function () {
-                global.location.href += "!";
-            }, 50);
-        };
-        global.onhashchange = function () {
-            if (global.location.hash !== _hash) {
-                global.location.hash = _hash;
-            }
-        };
-        global.onload = function () {
-            noBackPlease();
-        }
-    })(window);
+
 });
