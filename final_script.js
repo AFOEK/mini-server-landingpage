55b36726b0c1d9832ec4cc8302f159dbe276c309
AFOEK <felix.montalfu@gmail.com>
2021-08-26 17:03:42 +0700

    minor fix

--- final_script.js
+++ final_script.js
@@ -19,5 +19,30 @@
     }
 })(window);
 $(document).ready(function(){
-
+    canvas.oncontextmenu = function (e) {
+        e.preventDefault();
+    };
+    if(window.devtools.isOpen == true){
+        console.clear();
+            console.warn("Hey what you want doin here ? get back to your world");
+            console.warn("This place is reserved only for wizard...");
+            let str1 = "Privacy is the privacy, Why you still try hard to steal something you didn't deserve ?";
+            let str2 = "Privacy is sacred thing in this world, Why you destroy it ?";
+            let str3 = "Human is not great being, even we created computer but computer have great precision and fast";
+            let str4 = "Data is key for greater human future but it can be bullet to kill human"
+            random = Math.floor((Math.random()*4)+1);
+            if(random == 1){
+                str64 = btoa(unescape(encodeURIComponent(str1)))
+                console.warn(str64)
+            }else if(random == 2){
+                str64 = btoa(unescape(encodeURIComponent(str2)))
+                console.warn(str64)
+            }else if(random == 3){
+                str64 = btoa(unescape(encodeURIComponent(str3)))
+                console.warn(str64)
+            }else if(random == 4){
+                str64 = btoa(unescape(encodeURIComponent(str4)))
+                console.warn(str64)
+            }
+    }
 });
