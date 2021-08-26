208a2bd3dc46cec6499b3a6bbdf6f3dfc275a67d
AFOEK <felix.montalfu@gmail.com>
2021-08-26 16:10:02 +0700

    Adding devtools detection

--- script.js
+++ script.js
@@ -84,16 +84,15 @@ $(document).ready(function(){
         }, 3000);
     });
     $(".content_core").lettering('words');
-    $("body").keydown(function(e){
-        var key = e.KeyCode || e.which;
-        if(key == 123){
-            console.clear();
+    if(window.devtools.isOpen == true){
+        console.clear();
             console.warn("Hey what you want doin here ? get back to your world");
             console.warn("This place is reserved only for wizard...");
             let str1 = "Privacy is the privacy, Why you still try hard to steal something you didn't deserve ?";
             let str2 = "Privacy is sacred thing in this world, Why you destroy it ?";
             let str3 = "Human is not great being, even we created computer but computer have great precision and fast";
-            random = Math.floor((Math.random()*3)+1);
+            let str4 = "Data is key for greater human future but it can be bullet to kill human"
+            random = Math.floor((Math.random()*4)+1);
             if(random == 1){
                 str64 = btoa(unescape(encodeURIComponent(str1)))
                 console.warn(str64)
@@ -103,9 +102,11 @@ $(document).ready(function(){
             }else if(random == 3){
                 str64 = btoa(unescape(encodeURIComponent(str3)))
                 console.warn(str64)
+            }else if(random == 4){
+                str64 = btoa(unescape(encodeURIComponent(str4)))
+                console.warn(str64)
             }
-        }
-    });
+    }
     $(".question_place_1").click(function(){
         $(".answere_place_1").show(1000);
         setTimeout(function(){
